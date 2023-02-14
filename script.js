64d7c60b51c5bb631dd3579cd09d9ea03f794c1a
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-02-14 19:07:57 +0530

    first commit

--- script.js
+++ script.js
@@ -49,17 +49,22 @@ else {
 
 function addFunc(){
     var table = document.getElementById("table_body");
+    var tableCount = document.getElementById("table_body").row.length;
+    console.log(tableCount);
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
     cell1.innerHTML = '<input class="education_level" type="text" id="education_level" name="education" value="" placeholder="Education qualification">';
     cell2.innerHTML = '<input class="field" type="text" id="field" name="field" value="" placeholder="Field">';
     cell3.innerHTML = '<input class="year" type="text" id="year" name="year" value="" placeholder="Year">';
     cell4.innerHTML = '<input class="marks" type="text" name="marks" id="marks" value="" placeholder="Marks Obtained">';
+    cell5.innerHTML = '<button onclick="addFunc()" type="button" class="add" id="add" name="add" value="+"> + </button> <button onclick="myDeleteFunction()" type="button" class="add" id="add" name="add" value="-"> - </button>';
+
 }
 
-function myDeleteFunction() {
-  document.getElementById("myTable").deleteRow(0);
-}
+// function myDeleteFunction() {
+//   document.getElementById("myTable").deleteRow(0);
+// }
